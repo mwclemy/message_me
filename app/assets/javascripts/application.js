9158462faa7824d0a8f29870b5432919a503edc1
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2022-11-26 14:20:38 -0700

    added a favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+// Loads all Semantic javascripts
+//= require semantic-ui
+
+$(function () {
+    $('.ui.dropdown').dropdown();
+})
