8f0bc080bbc4ee73ae16d467a180afa004b28d71
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2022-11-26 22:36:27 -0700

    adde the user resource

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    validates :username, presence: true,
+                        uniqueness: true,
+                        length: {minimum: 3, maximum: 20}
+    has_secure_password
+end
