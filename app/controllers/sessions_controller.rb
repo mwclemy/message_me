30a22c3fc253d2da75d1f33e1a139784db5c1e43
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2022-11-30 22:20:19 -0700

    added controller level restrictions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    before_action :login_redirect, only: [:new, :create]
     def new
     end
     def create
@@ -16,5 +17,14 @@ class SessionsController < ApplicationController
         session[:user_id]= nil
         flash[:notice] = "You have successfully logged out"
         redirect_to login_url
+    end
+
+    private
+
+    def login_redirect
+        if logged_in
+            flash[:alert] = "You are already logged in"
+            redirect_to root_path
+        end
     end
 end
